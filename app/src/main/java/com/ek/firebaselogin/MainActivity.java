607250ae49687792e0680506931a02018f15f8b0
ekrusznis <ekrusznis@aol.com>
2019-02-27 09:40:30 -0500

    added nav drawer

--- app/src/main/java/com/ek/firebaselogin/MainActivity.java
+++ app/src/main/java/com/ek/firebaselogin/MainActivity.java
@@ -3,9 +3,14 @@ package com.ek.firebaselogin;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     String EmailHolder;
     TextView Email;
     Button LogOUT ;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    SwipeRefreshLayout swipeRefreshLayout;
+
 //    FirebaseAuth mAuth;
 //    FirebaseAuth.AuthStateListener mAuthListner;
 //    FirebaseUser mUser;
@@ -31,25 +41,47 @@ public class MainActivity extends AppCompatActivity {
 
         Email = (TextView)findViewById(R.id.textView1);
         LogOUT = (Button)findViewById(R.id.button1);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+        dl.addDrawerListener(t);
+        t.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.faves:
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        break;
+                    case R.id.settings:
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        break;
+                    case R.id.btnLogout:
+                        finish();
+                        Toast.makeText(MainActivity.this,"Log Out Successfull", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.btnAddNew:
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    default:
+
+                }
+                return true;
+            }
+        });
+
 
         Intent intent = getIntent();
-
         // Receiving User Email Send By MainActivity.
         EmailHolder = intent.getStringExtra(LoginActivity.userEmail);
-
         // Setting up received email to TextView.
         Email.setText(Email.getText().toString()+ EmailHolder);
-
         // Adding click listener to Log Out button.
-
         LogOUT.setOnClickListener(new View.OnClickListener() {
             // @Override
             public void onClick(View v) {
-
-
                 //Finishing current DashBoard activity on button click.
                 finish();
-
                 Toast.makeText(MainActivity.this,"Log Out Successfull", Toast.LENGTH_LONG).show();
                 //Intent intent=new Intent(DashboardActivity.this,login.class);
                 //startActivity(intent);
